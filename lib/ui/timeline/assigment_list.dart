f956740dd91d931ab6bf07c0fae754565e0858b0
Jayz Walker <odomchan06@gmail.com>
2019-03-30 09:09:39 +0700

    - code refactor

--- lib/ui/timeline/assigment_list.dart
+++ lib/ui/timeline/assigment_list.dart
@@ -1,126 +1,15 @@
-import 'package:flutter/material.dart';
 import '../../models/post_model.dart';
-import './post_item.dart';
 import '../../models/my_class_model.dart';
+import './post_list.dart';
 
-class AssignmentList extends StatelessWidget {
+class AssignmentList extends PostList {
   final Future<List<PostModel>> posts;
   final Future<MyClassModel> myClass;
 
-  AssignmentList({this.posts, this.myClass});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 244, 244, 244),
-      ),
-      child: FutureBuilder(future: Future(() async {
-        final data = await posts;
-        return data;
-      }), builder:
-          (BuildContext context, AsyncSnapshot<List<PostModel>> posts) {
-        if (posts.data == null) {
-          return Text("Loading....");
-        }
-        return ListView.builder(
-          itemCount: posts.data == null ? 1 : posts.data.length + 1,
-          itemBuilder: (BuildContext context, int index) =>
-              _itemBuilder(context, index, posts.data),
-        );
-      }),
-    );
-  }
-
-  Widget _itemBuilder(BuildContext context, int index, List<PostModel> posts) {
-    if (index == 0) return _headerBuilder();
-    index -= 1;
-    return PostItem(post: posts[index]);
-  }
-
-  Widget _headerBuilder() {
-    return Column(
-      children: <Widget>[
-        _buildClass(),
-      ],
-    );
-  }
-
-  Widget _buildClass() {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 20,
-        bottom: 60,
-        left: 10,
-        right: 10,
-      ),
-      color: Colors.purple,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          _buildTitleAndSub(),
-          _buildSetting(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTitleAndSub() {
-    return FutureBuilder(future: Future(() async {
-      final data = await myClass;
-      return data;
-    }), builder: (BuildContext context, AsyncSnapshot<MyClassModel> myClass) {
-      if (myClass.data == null) {
-        return Text('Loading...');
-      }
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            myClass.data.name,
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-                color: Colors.white),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8),
-            child: Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 30),
-                  child: Text(
-                    myClass.data.section,
-                    style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.white),
-                  ),
-                ),
-                Text(
-                  '${myClass.data.member} Member',
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white),
-                ),
-              ],
-            ),
-          )
-        ],
-      );
-    });
-  }
-
-  Widget _buildSetting() {
-    return GestureDetector(
-      onTap: () {},
-      child: Icon(
-        Icons.settings,
-        color: Colors.white,
-      ),
+  AssignmentList({this.posts, this.myClass}) {
+    PostList(
+      posts: posts,
+      myClass: myClass,
     );
   }
 }
--- lib/ui/timeline/timeline_screen.dart
+++ lib/ui/timeline/timeline_screen.dart
@@ -58,6 +58,7 @@ class _TimelineScreenState extends State<TimelineScreen>
         child: PostList(
           posts: widget.posts,
           myClass: widget.myClass,
+          shouldShowPostCreationInput: true,
         ),
       ),
       Container(
